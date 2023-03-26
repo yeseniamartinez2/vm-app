3ad93193f1137b73c0fd103c9aad0bfb7bd54d0a
Yesenia <a.yeseniamartinez@gmail.com>
2023-03-26 18:47:25 +0200

    unistalled express-oauth2-jwt-bearer

--- server.js
+++ server.js
@@ -4,31 +4,20 @@ const bodyParser = require("body-parser")
 const app = express()
 const routes = require("./routes")
 const dbo = require("./config/conn")
-// const { auth } = require("express-oauth2-jwt-bearer")
 require("dotenv").config()
 var cors = require("cors")
-const { requiredScopes } = require("express-oauth2-jwt-bearer")
 
 var corsOptions = {
     origin: "*",
     optionsSuccessStatus: 200, // For legacy browser support
 }
 
-// const checkJwt = auth({
-//     audience: process.env.AUDIENCE,
-//     issuerBaseURL: process.env.ISSUER_BASE_URL,
-// })
-
 app.use(cors(corsOptions))
 app.use(express.static("uploads"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 swaggerDocument = require("./swaggerDocument")
-// const checkScopes = requiredScopes("read:pets")
-// app.get("/role", checkJwt, checkScopes, (req, res) =>
-//     res.send("Read:pets permission")
-// )
-//app.get("/", checkJwt, (req, res) => res.send("App is secured"))
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use("/", routes)
 
